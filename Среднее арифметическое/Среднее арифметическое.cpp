8a4709a18db217cbc90bc8e6237b8fb9e3d73b11
Tw1ck11 <qwentyn2222222222@gmail.com>
2022-04-28 22:30:05 +0200

    Average

---
+++ Среднее арифметическое/Среднее арифметическое.cpp
@@ -0,0 +1,24 @@
+﻿// Среднее арифметическое.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num1, num2, num3, arithmetic_mean;
+
+    cout << "This program calculate arithmetic mean of three numbers" << endl;
+
+    cout << "Enter a first number: ";
+    cin >> num1;
+
+    cout << "Enter a second number: ";
+    cin >> num2;
+
+    cout << "Enter a third number: ";
+    cin >> num3;
+
+    arithmetic_mean = (num1 + num2 + num3) / 3;
+    cout << "Arithmetic mean of three numbers is equal to " << arithmetic_mean << endl;
+}
